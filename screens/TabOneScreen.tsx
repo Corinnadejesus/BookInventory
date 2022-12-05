ade75dac5916bf4f02f5c0d098584df1c4fb1e37
Corinna <Corinnadejesus@gmail.com>
2022-12-05 14:53:33 -0500

    Query books

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,21 +1,56 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
+import { ActivityIndicator, StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
+import { gql, useQuery } from "@apollo/client";
 
-export default function TabOneScreen({
-  navigation,
-}: RootTabScreenProps<"TabOne">) {
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
+
+export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "React Native" },
+  });
+
+  // console.log("data", data);
+  // console.log("loading", loading);
+  // console.log("error", error);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Screen One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {loading && <ActivityIndicator />}
+      {error && (
+        <>
+          <Text>Error Fetching Books</Text>
+          <Text>{error.message}</Text>
+        </>
+      )}
     </View>
   );
 }
