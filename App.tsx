131cee3f1df1d16b903c1cc31e27ffefbc1bb3c8
Corinna <Corinnadejesus@gmail.com>
2022-12-05 14:39:50 -0500

    Installed and set up Apollo Client

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+const API_KEY =
+  "santuario::stepzen.net+1000::97761f62cbee8cbe67f6ce10dbe5c95e8c5048f9de04f0b16b1e07bc2bccc531";
+
+const client = new ApolloClient({
+  uri: "https://santuario.stepzen.net/api/knotted-badger/__graphql",
+  headers: {
+    Authorization: `ApiKey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +26,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
